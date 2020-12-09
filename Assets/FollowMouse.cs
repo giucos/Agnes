c2be71adb55df5909d09f3208350ea5b6734ca79
giucos <75376131+giucos@users.noreply.github.com>
2020-12-09 12:33:44 +0100

    Mouse follows slow with space

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowMouse : MonoBehaviour
-{
+public class FollowMouse : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update() {
+    Vector3 mousePosition;
+    public float moveSpeed = 0.01f;
+    Rigidbody2D rb;
+    Vector2 position = new Vector2(0f, 0f);
 
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(cursorPos.x, cursorPos.y);
-        
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(position);
     }
 }
+
+
