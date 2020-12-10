d3c38c96fe4ddd909d04dc6c571f81d75d884bfa
giucos <75376131+giucos@users.noreply.github.com>
2020-12-10 13:21:22 +0100

    add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private float moveSpeed = 0.01f;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    Rigidbody2D rb;
+    Vector3 mousePosition;
+    Vector2 position = new Vector2(0f, 0f);
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Item")){
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
+
+
