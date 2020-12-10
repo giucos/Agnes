c1ce9e352a729d6bac7cf8c9c8e290db6c001120
siangi <siangi@gmx.ch>
2020-12-10 16:09:08 +0100

    Added Enemy and some Movement, but a part of the Maze is Pink Again

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyController : MonoBehaviour
+{
+    public float movementSpeed = 0.015f;
+    
+    private Rigidbody2D myRigidbody;
+
+    private int direction;
+    private static Vector3[] directions = { Vector3.up, Vector3.right, Vector3.down, Vector3.left };
+    Vector2 position = new Vector2(8.4f, -12.1f);
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        direction = 2; 
+    }
+
+    void Update()
+    {     
+    }
+
+    void OnCollisionEnter(Collision col){
+        direction = 3;
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.position += directions[direction] * movementSpeed;
+    }
+}
