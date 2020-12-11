dc44c690e765d78302bf72795a6b7baf0b1bd159
siangi <siangi@gmx.ch>
2020-12-11 14:07:34 +0100

    Collision with Enemy resets Scene, added a different Enemy behavior

---
+++ Assets/Scripts/EnemyControllerBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyControllerBase : MonoBehaviour
+{
+    private const int MAX_STOP_FRAME_COUNT = 20;
+    public float movementSpeed = 0.03f; 
+    internal int direction;
+    internal static Vector3[] directions = { Vector3.up, Vector3.right, Vector3.down, Vector3.left };
+    Vector2 position = new Vector2(8.4f, -12.1f);
+    private Vector3 oldPosition;
+    private int StillFrameCounter = 0;
+    
+    void Start()
+    {
+        direction = 0; 
+        oldPosition = position;
+    }
+
+    void Update()
+    {     
+        if (this.transform.position == oldPosition)  {
+            StillFrameCounter++;
+
+            if (StillFrameCounter >= MAX_STOP_FRAME_COUNT) {
+                setNewDirection();
+            }
+        } else {
+            oldPosition = this.transform.position;
+            StillFrameCounter = 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        setNewDirection();
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.position += directions[direction] * movementSpeed;
+    }
+
+    virtual public void setNewDirection(){
+        Debug.Log("Base Class setNewDirection entered");
+    }
+}
---
+++ Assets/Scripts/EnemyTurnRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurnRight : EnemyControllerBase
+{
+    private int directionChangeCounter = 0;
+    override public void setNewDirection(){
+        directionChangeCounter++;
+
+        if ((directionChangeCounter % 4) == 1){
+            if (direction >= 3){
+                direction = 0;
+            }
+            direction++;
+        } else {
+            if (direction <= 0){
+                direction = 3;
+            }
+            direction--;
+        }
+    }
+}
