73c33aec8dfb8ef4088ff8af83355b0f73e863b4
siangi <siangi@gmx.ch>
2020-12-16 14:39:59 +0100

    Added Sprint Script and Metas for the Character Assets

---
+++ Assets/Scripts/EnemySprint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySprint : EnemyControllerBase
+{
+    private int directionChangeCounter = 0; 
+
+    override public void setNewDirection(){
+        int oldDirection = direction;
+
+        do {
+            direction = Random.Range(0,4);
+        } while (direction == oldDirection);
+
+        if (directionChangeCounter % 10 == 0)
+        {
+            this.movementSpeed *= 1.5f;
+            Debug.Log("SpeedChange" + movementSpeed.ToString());
+        } else if(directionChangeCounter % 10 == 1)
+        {
+            this.movementSpeed /= 1.5f;
+            Debug.Log("SpeedBack" + movementSpeed.ToString());
+        }
+
+        directionChangeCounter++;
+    }
+}
