db4067fedc8b7ce4abbf2ea388181e16a1c1bd6d
giucos <75376131+giucos@users.noreply.github.com>
2020-12-17 14:43:05 +0100

    165

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,18 +20,27 @@ public class PlayerController : MonoBehaviour {
     Vector2 position = new Vector2(2.77f, -6.16f);
 
     public GameObject[] items;
-
+    
     void Start()
     {
         GameController = GetComponent<GameControl>();
         rb = GetComponent<Rigidbody2D>();
         items = GameObject.FindGameObjectsWithTag("Item");
+<<<<<<< Updated upstream
         blumentopfText.text = "1. Pflücke die Rose";
         gräsliText.text = "2. Ernte das Gras";
         giesskanneText.text = "3. Giesse die Blumen";
         kompostText.text = "4. Kompostiere den Abfall";
         rasenmäherText.text = "5. Mähe den Rasen";
         schaufelText.text = "6. Hole die Schaufel";  
+=======
+        blumentopfText.text = "1. Rose";
+        gräsliText.text = "2. Gras";
+        giesskanneText.text = "3. Giessekanne";
+        kompostText.text = "4. Kompost";
+        rasenmäherText.text = "5. Rasenmäher";
+        schaufelText.text = "6. Schaufel";   
+>>>>>>> Stashed changes
 
     }
 
@@ -40,6 +49,7 @@ public class PlayerController : MonoBehaviour {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed);
+        
     }
 
     void FixedUpdate()
@@ -54,6 +64,9 @@ public class PlayerController : MonoBehaviour {
             if (items[itemCounter].name == other.name && itemCounter < items.Length)
             {
                 other.gameObject.SetActive(false);
+                SoundManager.PlaySound("item");
+                SoundManager.MuteMainTheme();
+                //SoundManager.PlaySound("gameOver");
                 CheckItemList();               
                 itemCounter++;
             }
@@ -71,22 +84,22 @@ public class PlayerController : MonoBehaviour {
     { 
         if (itemCounter == 0)
         {
-            blumentopfText.text = "1. Pflücke die Rose" + "\u2714";
+            blumentopfText.text = "1. Rose" + "\u2714";
         } else if (itemCounter == 1)
         {
-            gräsliText.text = "2. Ernte das Gras" + "\u2714";
+            gräsliText.text = "2. Gras" + "\u2714";
         } else if (itemCounter == 2)
         {
-            giesskanneText.text = "3. Giesse die Blumen" + "\u2714";
+            giesskanneText.text = "3. Giessekanne" + "\u2714";
         } else if (itemCounter == 3)
         {
-            kompostText.text = "4. Kompostiere den Abfall" + "\u2714";
+            kompostText.text = "4. Kompost" + "\u2714";
         } else if (itemCounter == 4)
         {
-            rasenmäherText.text = "5. Mähe den Rasen" + "\u2714";
+            rasenmäherText.text = "5. Rasenmäher" + "\u2714";
         } else if (itemCounter == 5)
         {
-            schaufelText.text = "6. Hole die Schaufel" + "\u2714";
+            schaufelText.text = "6. Schaufel" + "\u2714";
         }
     }
 }
